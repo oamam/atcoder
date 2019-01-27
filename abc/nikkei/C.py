ac648c3a7d9353076ba92796cd56dbbc9637ba00
hiamao <chapa0106@gmail.com>
2019-01-27 23:52:54 +0900

    AC

--- abc/nikkei/C.py
+++ abc/nikkei/C.py
@@ -1,30 +1,14 @@
 def main():
     N = int(input())
-    a = []
-    b = []
-    for _ in range(N):
-        ai, bi = map(int, input().split())
-        a.append(ai)
-        b.append(bi)
+    ab = [[int(i) for i in input().split()] for _ in range(N)]
+    ab = sorted(ab, key=lambda x: x[0] + x[1], reverse=True)
     ans = 0
-    s = 1
-    while True:
-        if sum(a) == 0:
-            print(ans)
-            exit()
-        ma = max(a)
-        mb = max(b)
-        ans += ma * s
-        if ma >= mb:
-            i = a.index(ma)
+    for i in range(N):
+        if i % 2 == 0:
+            ans += ab[i][0]
         else:
-            i = b.index(mb)
-        a[i] = 0
-        b[i] = 0
-        t = a
-        a = b
-        b = t
-        s *= -1
+            ans -= ab[i][1]
+    print(ans)
 
 
 main()
