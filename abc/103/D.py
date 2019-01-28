fc1783362c8f258bcba277d31f604db799cb5400
hiamao <chapa0106@gmail.com>
2019-01-29 08:24:04 +0900

    AC

--- abc/103/D.py
+++ abc/103/D.py
@@ -1,17 +1,14 @@
 def main():
     N, M = map(int, input().split())
     ab = [[int(i) for i in input().split()] for _ in range(M)]
-    ab = sorted(ab, key=lambda x: x[1] - x[0])
-    d = [ab.pop(0)]
+    ab = sorted(ab, key=lambda x: x[1])
+    last = ab[0][1] - 1
     ans = 1
-    for a, b in ab:
-        flg = True
-        for aa, bb in d:
-            if b > aa or bb > a:
-                flg = False
-                break
-        if flg is True:
-            ans += 1
+    for a, b in ab[1:]:
+        if last >= a:
+            continue
+        ans += 1
+        last = b - 1
     print(ans)
 
 
