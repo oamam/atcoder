e0770dcfe494a279a3ff93ebf226e3074e0f3ac8
hiamao <chapa0106@gmail.com>
2019-02-02 21:39:55 +0900

    AC

--- abc/99/C.py
+++ abc/99/C.py
@@ -1,20 +1,17 @@
 def main():
     N = int(input())
-    ans = 0
-
-    while N >= 6:
-        i = 0
-        j = 0
-        while N > 9 ** (i + 1):
-            i += 1
-        while N > 6 ** (j + 1):
-            j += 1
-        m = max(9 ** i, 6 ** j)
-        print(N, m)
-        ans += N // m
-        N %= m
-
-    print(ans, N)
+    dp = [N] * (N + 1)
+    dp[0] = 0
+    for n in range(1, N + 1):
+        i = 1
+        while n >= i:
+            dp[n] = min(dp[n], dp[n - i] + 1)
+            i *= 6
+        j = 1
+        while n >= j:
+            dp[n] = min(dp[n], dp[n - j] + 1)
+            j *= 9
+    print(dp[N])
 
 
 main()
