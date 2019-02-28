c487b65dea88fdd994fac249f08b126750a6d2ed
oamam <chapa0106@gmail.com>
2019-02-28 23:17:24 +0900

    AC

--- abc/91/C.py
+++ abc/91/C.py
@@ -1,21 +1,22 @@
-def f(blue, red, ans):
-    c = 0
-    for i, b in enumerate(blue):
-        for j, r in enumerate(red):
-            if b[0] >= r[0] and b[1] >= r[1]:
-                blue.pop(i)
-                red.pop(j)
-                c = max(c, f(blue, red, ans + 1))
-                blue.insert(i, b)
-                red.insert(j, r)
-    return max(c, ans)
-
-
 def main():
     N = int(input())
-    red = [list(map(int, input().split())) for _ in range(N)]
-    blue = [list(map(int, input().split())) for _ in range(N)]
-    print(f(blue, red, 0))
+    red = sorted(
+        [list(map(int, input().split())) for _ in range(N)],
+        key=lambda x: x[1],
+        reverse=True
+    )
+    blue = sorted(
+        [list(map(int, input().split())) for _ in range(N)],
+        key=lambda x: x[0]
+    )
+    ans = 0
+    for b in blue:
+        for i, r in enumerate(red):
+            if b[0] > r[0] and b[1] > r[1]:
+                ans += 1
+                red.pop(i)
+                break
+    print(ans)
 
 
 main()
