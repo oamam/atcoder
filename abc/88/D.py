4e256e9cdbcdbc61837efcb67dcfca195fb53aa9
oamam <chapa0106@gmail.com>
2019-03-24 10:17:45 +0900

    AC bfs

--- abc/88/D.py
+++ abc/88/D.py
@@ -1,29 +1,31 @@
 H, W = map(int, input().split())
 S = [input() for _ in range(H)]
-M = {(0, 0): 1}
 
 
-def dfs(x, y, c):
-    cc = float('inf')
-    for xx, yy in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
-        if (H, W) == (yy + 1, xx + 1):
-            return c + 1
-        if 0 <= yy < H and 0 <= xx < W and S[yy][xx] == '.' and \
-                ((xx, yy) not in M or M[(xx, yy)] > c + 1):
-            M[(xx, yy)] = c + 1
-            cc = min(cc, dfs(xx, yy, c + 1))
-    return cc
+def bfs():
+    q = [(0, 0, 1)]
+    m = [[0 for _ in range(W)] for _ in range(H)]
+    while len(q) > 0:
+        x, y, c = q.pop(0)
+        for xx, yy in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
+            if (H, W) == (yy + 1, xx + 1):
+                return c + 1
+            if 0 <= yy < H and 0 <= xx < W and S[yy][xx] == '.' and \
+                    m[yy][xx] == 0:
+                q.append((xx, yy, c + 1))
+                m[yy][xx] = 1
+    return -1
 
 
 def main():
-    ans = 0
+    dc = 0
     for i in range(H):
-        ans += S[i].count('.')
-    c = dfs(0, 0, 1)
-    if c == float('inf'):
+        dc += S[i].count('.')
+    c = bfs()
+    if c == -1:
         print(-1)
     else:
-        print(ans - c)
+        print(dc - c)
 
 
 main()
