326868699631281165aa342c592d71e0b2f3a3d2
oamam <chapa0106@gmail.com>
2019-05-08 09:10:30 +0900

    AC

--- abc/82/B.py
+++ abc/82/B.py
@@ -1,14 +1,8 @@
 def main():
-    c = 'abcdefghijklmnopqrstuvwxyz'
-    x = sorted(input())
-    y = sorted(input(), reverse=True)
-    if x == y[:len(x)] and len(y) > len(x):
+    if sorted(input()) < sorted(input(), reverse=True):
         print('Yes')
     else:
-        if c.index(x[0]) >= c.index(y[0]):
-            print('No')
-        else:
-            print('Yes')
+        print('No')
 
 
 main()
