db11c895af96762fbc61a8a474d2f77d2e8fd024
oamam <chapa0106@gmail.com>
2019-05-23 07:22:40 +0900

    AC

--- abc/80/C.py
+++ abc/80/C.py
@@ -1,15 +1,34 @@
-def main():
-    N = int(input())
-    F = [int(input().replace(' ', ''), 2) for _ in range(N)]
-    P = [list(map(int, input().split())) for _ in range(N)]
+N = int(input())
+F = []
+P = []
+for _ in range(N):
+    F.append(list(map(int, input().split())))
+for _ in range(N):
+    P.append(list(map(int, input().split())))
+a = [0] * 20
 
-    ans = -10 ** 10
-    for i in range(1, 2 ** 10):
-        c = 0
+
+def dfs(pos):
+    if pos == 10:
+        if a.count(0) == 20:
+            return -1000000000
+        score = 0
         for f, p in zip(F, P):
-            c += p[bin(f & i).count('1')]
-        ans = max(ans, c)
-    print(ans)
+            c = 0
+            for aa, ff in zip(a, f):
+                if aa == ff == 1:
+                    c += 1
+            score += p[c]
+        return score
+    a[pos] = 0
+    x = dfs(pos + 1)
+    a[pos] = 1
+    y = dfs(pos + 1)
+    return max(x, y)
+
+
+def main():
+    print(dfs(0))
 
 
 main()
