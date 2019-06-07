23156163540c0b44d120482ba93d485a8cef6a4f
oamam <chapa0106@gmail.com>
2019-06-07 22:44:16 +0900

    AC

--- abc/79/D.py
+++ abc/79/D.py
@@ -1,33 +1,16 @@
-H, W = map(int, input().split())
-C = [list(map(int, input().split())) for _ in range(10)]
-A = [list(map(int, input().split())) for _ in range(H)]
-
-
-def f(i, s, m):
-    if i == 1:
-        return s
-    a = float('INF')
-    for j, k in enumerate(C[i]):
-        print(s, a)
-        if s >= a:
-            print('ok')
-            break
-        if m[j] is True:
-            continue
-        m[j] = True
-        a = min(a, f(j, s + k, m))
-        m[j] = False
-    return a
-
-
 def main():
+    H, W = map(int, input().split())
+    C = [list(map(int, input().split())) for _ in range(10)]
+    A = [list(map(int, input().split())) for _ in range(H)]
+    for k in range(10):
+        for i in range(10):
+            for j in range(10):
+                C[i][j] = min(C[i][j], C[i][k] + C[k][j])
     ans = 0
     for h in range(H):
-        for i in A[h]:
-            if i > 1:
-                m = [False] * 10
-                m[i] = True
-                ans += f(i, 0, m)
+        for a in A[h]:
+            if a >= 0:
+                ans += C[a][1]
     print(ans)
 
 
