4a055a1d46e3044994a30f3b9c2096c8faf05835
oamam <chapa0106@gmail.com>
2019-06-10 23:40:04 +0900

    AC

--- dp/A.py
+++ dp/A.py
@@ -1,13 +1,13 @@
 def main():
     N = int(input())
     h = list(map(int, input().split()))
-    dp = [0 for _ in range(N)]
+    dp = [0] * N
     dp[1] = abs(h[0] - h[1])
     for i in range(2, N):
-        h1 = dp[i - 1] + abs(h[i] - h[i - 1])
-        h2 = dp[i - 2] + abs(h[i] - h[i - 2])
-        dp[i] = min(h1, h2)
-
+        dp[i] = min(
+            abs(h[i] - h[i - 2]) + dp[i - 2],
+            abs(h[i] - h[i - 1]) + dp[i - 1]
+        )
     print(dp[-1])
 
 
--- dp/B.py
+++ dp/B.py
@@ -1,14 +1,12 @@
 def main():
     N, K = map(int, input().split())
     h = list(map(int, input().split()))
-    dp = [float('inf') for _ in range(N)]
+    dp = [float('INF') for _ in range(N)]
     dp[0] = 0
     dp[1] = abs(h[0] - h[1])
     for i in range(2, N):
-        k = 1
         for k in range(1, K + 1):
             dp[i] = min(dp[i], dp[i - k] + abs(h[i] - h[i - k]))
-
     print(dp[-1])
 
 
