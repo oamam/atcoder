e0810ce6004ecbaae2bf5acee6a2218020cd5956
oamam <chapa0106@gmail.com>
2019-06-11 12:58:17 +0900

    AC

--- dp/C.py
+++ dp/C.py
@@ -1,21 +1,19 @@
 def main():
     N = int(input())
-    a = [0]
-    b = [0]
-    c = [0]
+    A = []
+    B = []
+    C = []
     for _ in range(N):
-        ai, bi, ci = map(int, input().split())
-        a.append(ai)
-        b.append(bi)
-        c.append(ci)
-    dp = [[0] * 3 for _ in range(N + 1)]
-
-    for i in range(1, N + 1):
-        dp[i][0] = max(dp[i - 1][1], dp[i - 1][2]) + a[i]
-        dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + b[i]
-        dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + c[i]
-
-    print(max(dp[N]))
+        a, b, c = map(int, input().split())
+        A.append(a)
+        B.append(b)
+        C.append(c)
+    dp = [[0] * 3 for _ in range(N)]
+    for i in range(1, N):
+        dp[i][0] = A[i] + max(dp[i - 1][1], dp[i - 1][2])
+        dp[i][1] = B[i] + max(dp[i - 1][0], dp[i - 1][2])
+        dp[i][2] = C[i] + max(dp[i - 1][0], dp[i - 1][1])
+    print(max(dp[-1]))
 
 
 main()
