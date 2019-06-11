ad608a5f8ecda4bc7e31918d45ec43dccdb655da
oamam <chapa0106@gmail.com>
2019-06-11 23:30:25 +0900

    AC

--- dp/D.py
+++ dp/D.py
@@ -1,19 +1,20 @@
 def main():
     N, W = map(int, input().split())
-    wl = []
-    vl = []
+    WV = []
     for _ in range(N):
         w, v = map(int, input().split())
-        wl.append(w)
-        vl.append(v)
+        WV.append((w, v))
     dp = [[0] * (W + 1) for _ in range(N + 1)]
     for i in range(N):
         for j in range(W + 1):
-            if wl[i] > j:
-                dp[i + 1][j] = dp[i][j]
+            if j >= WV[i][0]:
+                dp[i + 1][j] = max(
+                    dp[i][j],
+                    dp[i][j - WV[i][0]] + WV[i][1]
+                )
             else:
-                dp[i + 1][j] = max(dp[i][j], dp[i][j - wl[i]] + vl[i])
-    print(dp[N][W])
+                dp[i + 1][j] = dp[i][j]
+    print(dp[-1][-1])
 
 
 main()
