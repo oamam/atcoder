3c38d36dbc71533c32bc43b50fef8e60b02c3c2e
oamam <chapa0106@gmail.com>
2019-06-24 21:52:16 +0900

    AC

--- abc/71/C.py
+++ abc/71/C.py
@@ -1,19 +1,14 @@
 def main():
     N = int(input())
-    A = sorted(list(map(int, input().split())), reverse=True)
+    A = sorted(list(map(int, input().split())))
     i = 0
-    d = []
+    d = [0, 0]
     while N - 1 > i:
-        if len(d) == 2:
-            break
         if A[i] == A[i + 1]:
             d.append(A[i])
             i += 1
         i += 1
-    if (len(d) == 2):
-        print(d[0] * d[1])
-    else:
-        print(0)
+    print(d[-1] * d[-2])
 
 
 main()
