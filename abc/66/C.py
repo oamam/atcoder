2edf66eb955b16ef07967d97c13034daabe7ed72
oamam <chapa0106@gmail.com>
2019-07-06 16:25:07 +0900

    AC

--- abc/66/C.py
+++ abc/66/C.py
@@ -1,20 +1,10 @@
 def main():
     n = int(input())
     A = input().split()
-    B = [0] * n
-    k = n % 2
-    l = n // 2
-    if k == 0:
-        l -= 1
-    r = l + 1
-    for i in range(n):
-        if i % 2 != k:
-            B[l] = A[i]
-            l -= 1
-        else:
-            B[r] = A[i]
-            r += 1
-    print(' '.join(B))
+    if n % 2 == 0:
+        print(' '.join(A[1::2][::-1] + A[0::2]))
+    else:
+        print(' '.join(A[0::2][::-1] + A[1::2]))
 
 
 main()
