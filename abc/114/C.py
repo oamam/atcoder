2d86f87a0f62dedd0b070c7fbd52497b9c13067e
oamam <chapa0106@gmail.com>
2019-09-14 17:30:37 +0900

    AC

--- abc/114/C.py
+++ abc/114/C.py
@@ -1,24 +1,16 @@
-N = input()
+N = int(input())
+ans = 0
 
 
-def check(n, ln):
-    if ln == 0:
-        if int(N) >= int(n) and all(n.count(s) > 0 for s in '357'):
-            return 1
-        else:
-            return 0
-    cnt = 0
-    for i in '357':
-        cnt += check(n + i, ln - 1)
-    return cnt
+def fn(x):
+    global ans
+    if int(x) > N:
+        return
+    if all(x.count(i) for i in '753'):
+        ans += 1
+    for i in '753':
+        fn(x + i)
 
 
-def main():
-    cnt = 0
-    for l in range(3, len(N) + 1):
-        cnt += check('', l)
-    print(cnt)
-
-
-if __name__ == '__main__':
-    main()
+fn('0')
+print(ans)
--- abc/114/D.py
+++ abc/114/D.py
@@ -1,23 +1,20 @@
-def check(m, e):
+N = int(input())
+e = [0] * (N + 1)
+for i in range(2, N + 1):
+    c = i
+    for j in range(2, i + 1):
+        while c % j == 0:
+            e[j] += 1
+            c //= j
+
+
+def fn(m):
     return len(list(filter(lambda x: x >= m - 1, e)))
 
 
-def main():
-    N = int(input())
-    e = [0] * (N + 1)
-
-    for i in range(2, N + 1):
-        ii = i
-        for j in range(2, i + 1):
-            while ii % j == 0:
-                e[j] += 1
-                ii //= j
-
-    print(check(75, e) +
-          check(25, e) * (check(3, e) - 1) +
-          check(15, e) * (check(5, e) - 1) +
-          check(5, e) * (check(5, e) - 1) * (check(3, e) - 2) // 2)
-
-
-if __name__ == '__main__':
-    main()
+print(
+    fn(75) +
+    fn(25) * (fn(3) - 1) +
+    fn(15) * (fn(5) - 1) +
+    fn(5) * (fn(5) - 1) * (fn(3) - 2) // 2
+)
